1fde57e270b620769caaa8f04ddf0011e9450921
unknown <Lautaro Ferreyra>
2022-08-14 21:20:54 -0300

    changed url backend

--- PF-G1-FRONTEND-main/henry-diamond/src/config.js
+++ PF-G1-FRONTEND-main/henry-diamond/src/config.js
@@ -1,29 +1,27 @@
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 dotenv.config();
 
+const CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
+const URL =
+  process.env.BACK || "https://pf-g1-backend-production.up.railway.app/";
+// const URL = process.env.BACK || process.env.back || "http://localhost:9000/";
 
-  const CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
-  const URL = process.env.BACK || process.env.back || "https://pf-g1-backend-production-3e79.up.railway.app/";
-  // const URL = process.env.BACK || process.env.back || "http://localhost:9000/";
-  
+if (!CLIENT_ID) {
+  throw new Error("falta env.clientid");
+}
 
-  if (!CLIENT_ID) {
-    throw new Error("falta env.clientid")
-  }
+if (!AUTH0_DOMAIN) {
+  throw new Error("falta env.domain");
+}
 
-  if (!AUTH0_DOMAIN) {
-    throw new Error("falta env.domain")
-  }
+if (!URL) {
+  console.log(URL);
+  throw new Error("falta env.URL");
+}
 
-  if (!URL) {
-    console.log(URL)
-    throw new Error("falta env.URL")
-  }
-
-
-  module.exports = {
-    CLIENT_ID,
-    AUTH0_DOMAIN,
-    URL
-  }
+module.exports = {
+  CLIENT_ID,
+  AUTH0_DOMAIN,
+  URL,
+};
